6038dc2d9584dc2dd9aa4ca5e2c513395815d61b
Bhanu Pratap <167957524+BhanuPratap13@users.noreply.github.com>
2026-02-09 19:08:23 +0530

    Time: 1429 ms (5.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 178-rank-scores/rank-scores.sql
@@ -0,0 +1,9 @@
+select 
+    s1.score,
+    (
+        select count(distinct s2.score)
+        from Scores s2
+        where s2.score > s1.score
+    ) + 1 AS 'rank'
+from scores s1
+order by s1.score desc    
